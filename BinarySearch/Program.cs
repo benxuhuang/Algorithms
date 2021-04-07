5a0df23575b01aa16737ea523cc4f488c840d09f
Ben Huang <benxu.huang@gmail.com>
2021-04-07 10:31:38 +0800

    add binary search and linear search

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Console.WriteLine(BinarySearch(arr,2));
+        }
+
+        static int BinarySearch(int[] arr, int target)
+        {
+            int start = 0, end = arr.Length-1;
+
+            while (end >= start)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (target == arr[mid])
+                    return mid;
+            
+                if (target > arr[mid])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LinearSearch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 4,1,7,1,7,5,1,8,4,2 };
+            Console.WriteLine(LinearSearch(arr,8));
+        }
+
+        static int LinearSearch(int[] arr, int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
